68fb2f1b00453a1fc055c95ed1a6c440c63bde50
Aymane <aymane.larhrissi1@gmail.com>
2026-05-05 18:01:51 +0100

    Added addEntity and removeEntity for Sector class

--- include/Sector.h
+++ include/Sector.h
@@ -10,14 +10,16 @@
 #include "SpaceEntity.h"
 
 class Sector {
+    friend class Render;
 private:
-    int x_start, y_start;
-    int x_end, y_end;
-    std::vector<SpaceEntity*> entities;
+    int _x_start, _y_start;
+    int _x_end, _y_end;
+    std::vector<SpaceEntity*> _entities;
 public:
     Sector(int x_s, int x_e, int y_s, int y_e);
     ~Sector();
-
+    void addEntity(SpaceEntity* entity);
+    void removeEntity(SpaceEntity* entity);
 };
 
 
--- src/Sector.cpp
+++ src/Sector.cpp
@@ -4,8 +4,24 @@
 
 #include "../include/Sector.h"
 
-Sector::Sector(const int x_s, const int x_e, const int y_s, const int y_e) : x_start(x_s), y_start(y_s), x_end(x_e),  y_end(y_e) {};
+Sector::Sector(const int x_s, const int x_e, const int y_s, const int y_e) : _x_start(x_s), _y_start(y_s), _x_end(x_e),  _y_end(y_e) {};
 
-Sector::~Sector() = default;
+Sector::~Sector()
+{
+    for (const SpaceEntity* entity : _entities)
+        delete entity;
+}
+
+void Sector::addEntity(SpaceEntity* entity)
+{
+    _entities.push_back(entity);
+}
+
+void Sector::removeEntity(SpaceEntity* entity)
+{
+    std::erase(_entities, entity);
+}
+
+
 
 
