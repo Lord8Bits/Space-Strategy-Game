c3ac94d93108e5b3e4310f899f214a6b7dbed40b
Aymane <aymane.larhrissi1@gmail.com>
2026-05-05 22:47:31 +0100

    added new coordTranslation function to convert sector coordinates to world coordinates. And corrected a draw() logic bug as well as improve upon the _frame_buffer logic and formatting.

--- src/Render.cpp
+++ src/Render.cpp
@@ -18,7 +18,7 @@ void Render::draw(const Map& map)
 {
     const Sector& sector = map._sectors[map._selected_sector];
 
-    std::fill_n(_world.begin() + sector._x_start*sector._y_start, VIEWPORT_HEIGHT*VIEWPORT_WIDTH, Cell{'.', White});
+    std::fill_n(_world.begin() + sector._y_start * VIEWPORT_WIDTH + sector._x_start, VIEWPORT_HEIGHT*VIEWPORT_WIDTH, Cell{'.', White});
 
     for (const SpaceEntity* entity : sector._entities) {
         if (entity == nullptr) continue;
@@ -30,22 +30,27 @@ void Render::draw(const Map& map)
         _world[idx] = entity->_cell;
     }
 
-    const int start{sector._x_start * sector._y_start};
-    const int end{sector._x_end * sector._y_end};
 
-    Color last_color{_world[start].color};
-    _frame_buffer = "\033[H";
+    Color last_color{White};
+    constexpr std::string_view return_code{"\033[H"};
+
+    _frame_buffer =  return_code;
     _frame_buffer += colorFormat(last_color);
 
-    for (int i = start; i < end; i++) {
-        if (last_color != _world[i].color){
-            _frame_buffer += colorFormat(_world[i].color);
-            last_color = _world[i].color;
+    for (int y = 0; y < VIEWPORT_HEIGHT; y++) {
+        for (int x = 0; x < VIEWPORT_WIDTH; x++){
+            const int idx{coordTranslation(x, y, map._selected_sector)};
+
+            if (last_color != _world[idx].color) {
+                _frame_buffer += colorFormat(_world[idx].color);
+                last_color = _world[idx].color;
+            }
+            _frame_buffer += _world[idx].symbol;
         }
-        _frame_buffer += _world[i].symbol;
+        _frame_buffer += '\n';
     }
 
-    _frame_buffer += "\x1b[0m";
+    _frame_buffer += colorFormat(White);
 
     std::cout << _frame_buffer;
     std::cout.flush();
@@ -67,3 +72,7 @@ std::string_view Render::colorFormat(const Color color)
     }
 }
 
+int Render::coordTranslation(const int x, const int y, const int selected_sector)
+{
+    return (y * VIEWPORT_WIDTH + x) + selected_sector * (VIEWPORT_HEIGHT*VIEWPORT_WIDTH);
+}
