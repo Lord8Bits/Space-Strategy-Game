68786bf0853b6fa1ff144ac0a4baaeeecdb9fa67
yassierr <najahnajah302@gmail.com>
2026-05-07 14:07:22 +0100

    Create Resource.cpp
    implement Resource class methods

---
+++ Resource.cpp
@@ -0,0 +1,48 @@
+#include "Resource.h"
+//Constructor
+Resource::Resource(int g, int s, int so) {
+    gold = g;
+    silver = s;
+    sodium = so;
+}
+//Getters
+
+int Resource::getGold() {
+    return gold;
+}
+int Resource::getSilver() {
+    return silver;
+}
+int Resource::getSodium() {
+    return sodium;
+}
+//Add functions
+
+void Resource::addGold(int value) {
+    gold += value;
+}
+
+void Resource::addSilver(int value) {
+    silver += value;
+}
+
+void Resource::addSodium(int value) {
+    sodium += value;
+}
+// Consume functions
+
+void Resource::consumeGold(int value) {
+   if (gold >= value) {  // So that the value doesnt become negative
+    gold -= value;
+   }
+}
+void Resource::consumeSilver(int value) {
+   if (silver >= value) {
+    silver -= value;
+   }  
+}
+void Resource::consumeSodium(int value) {
+  if (sodium >= value) {
+    sodium -= value;
+  }
+}
