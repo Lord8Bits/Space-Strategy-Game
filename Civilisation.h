54504d92ce08042fcbb54879060cd0f8e144cffb
yassierr <najahnajah302@gmail.com>
2026-05-16 02:00:05 +0100

    Create Civilisation.h
    Entering the attributes of the Class

---
+++ Civilisation.h
@@ -0,0 +1,21 @@
+#ifndef CIVILIZATION_H
+#define CIVILIZATION_H
+
+#include <string>
+#include <vector>
+
+#include "Resource.h"
+#include "Planet.h"
+#include "Updatable.hpp"
+
+using namespace std;
+
+// represent a civilisation in the game
+class Civilization : public Updatable {
+
+private:
+    // Attributes
+    string name;          // civilization name
+    Resource resources;         // civilization resources
+    vector<Planet> planets;    // owned planets
+public :
