70bb77b71edd16b8bb4d97f65ab84becd66f13a5
yassierr <najahnajah302@gmail.com>
2026-05-23 12:26:12 +0100

    Update main.cpp
    create planet , resources

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "Resource.h"
+#include "Planet.h"
+#include "Civilization.h"
 
+using namespace std;
+
+int main() {
+    // Create Resources
+    Resource earthResources(100, 50, 25);
+    Resource marsResources(80, 40, 10);
+    Resource civilizationResources(500, 300, 100);
+    // Create Planets
+    Planet earth("Earth", earthResources, false);
+    Planet mars("Mars", marsResources, false);
