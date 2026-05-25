06937a6df91b84f76bf9678e97fa3ded5f42554d
yassierr <najahnajah302@gmail.com>
2026-05-25 14:18:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,29 +12,29 @@ int main() {
 
     cout << "Gold : " << resources.getGold() << endl;
     cout << "Silver : " << resources.getSilver() << endl;
-    cout << "Sodium : " << resources.getSodium() << endl;
+    cout << "Sodium : " << resources.getdiamond() << endl;
 
     // Add resources
     resources.addGold(20);
     resources.addSilver(10);
-    resources.addSodium(5);
+    resources.adddiamond(5);
     cout << endl;
     
     cout << "AFTER ADDING RESOURCES" << endl;
     cout << "Gold : " << resources.getGold() << endl;
     cout << "Silver : " << resources.getSilver() << endl;
-    cout << "Sodium : " << resources.getSodium() << endl;
+    cout << "Sodium : " << resources.getdiamond() << endl;
 
     // Consume resources
     resources.consumeGold(30);
     resources.consumeSilver(15);
-    resources.consumeSodium(10);
+    resources.consumediamond(10);
     cout << endl;
     
     cout << "AFTER CONSUMING RESOURCES" << endl;
     cout << "Gold: " << resources.getGold() << endl;
     cout << "Silver: " << resources.getSilver() << endl;
-    cout << "Sodium: " << resources.getSodium() << endl;
+    cout << "Sodium: " << resources.getdiamond() << endl;
     resources.update(); // And finally the update
     return 0;
 }
