377c7afd81bd6101b119efb725baf2f504f2b153
yassierr <najahnajah302@gmail.com>
2026-05-25 14:08:29 +0100

    Update Civilization.hpp

--- Civilization.hpp
+++ Civilization.hpp
@@ -2,7 +2,7 @@
 #define CIVILIZATION_H
 
 #include <string>
-#include <vector>
+#include <map>
 
 #include "Resource.hpp"
 #include "Planet.hpp"
@@ -10,50 +10,43 @@
 
 using namespace std;
 
-// represent a civilisation in the game
 class Civilization : public Updatable {
 
 private:
-    // Attributes
-    string name;          // civilization name
-    Resource resources;         // civilization resources
-    vector<Planet> planets;    // owned planets
-public :
-    
-    // Initializes civilization information
+
+    string name;
+    Resource resources;
+
+    // key = planet name
+    // value = Planet object
+    map<string, Planet> planets;
+
+public:
+
     Civilization(string n, Resource r);
 
-        // GETTERS
+    // GETTERS
     string getName();
-    // returns civilization name
     Resource getResources();
-    // returns civilization resources
     int getPlanetCount();
-    // returns the number of planets owned by the civilization
-    vector<Planet> getPlanets();
-    // returns civilization planets
 
-        // SETTERS
+    map<string, Planet> getPlanets();
+
+    // SETTERS
     void setName(string n);
-    // changes civilization name
 
-        // PLANET MANAGEMENT
+    // PLANET MANAGEMENT
     void addPlanet(Planet p);
-    // adds a planet to the civilization
-    void removePlanet(int index);
-    // removes a planet using its index
+    void removePlanet(string planetName);
+    Planet* findPlanet(string planetName);
 
-       // RESOURCE MANAGEMENT
+    // RESOURCE MANAGEMENT
     void addGold(int value);
-    // adds gold to civilization resources
     void addSilver(int value);
-    // adds silver to civilization resources
     void addSodium(int value);
-    // adds sodium to civilization resources
 
-      // UPDATE FUNCTION
+    // UPDATE
     void update() override;
-    // updates civilization information
 };
 
 #endif
