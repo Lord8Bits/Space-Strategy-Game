2a844c93fd40f1ec7269d6af26d7e7a8a413eec0
yassierr <najahnajah302@gmail.com>
2026-05-26 17:29:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,86 +3,23 @@
 
 using namespace std;
 
-int main() {
-
-    // Create a technology
-
+int main()
+{
     Technology mining(1, "Mining Technology", 1, 5, 100);
 
-    // Display initial information
-
-    cout << "INITIAL TECHNOLOGY" << endl;
-
     mining.displayInfo();
-
-    // Unlock technology
-
-    cout << "\nUnlocking technology..." << endl;
 
     mining.unlock();
 
-    mining.displayInfo();
-
-    // Upgrade technology
-
-    cout << "\nUpgrading technology..." << endl;
-
     mining.upgrade();
 
+    cout << "\nAfter upgrade:\n";
+
     mining.displayInfo();
 
-    // Check if upgrade is possible
-
-    cout << "\nCan Upgrade: ";
-
-    if (mining.canUpgrade()) {
-        cout << "Yes" << endl;
-    }
-    else {
-        cout << "No" << endl;
-    }
-
-    // Display upgrade cost
-
-    cout << "\nNext Upgrade Cost: "
-         << mining.calculateUpgradeCost()
-         << endl;
-
-    // Display bonuses
-
-    cout << "\n===== BONUSES =====" << endl;
-
-    cout << "Production Bonus: "
+    cout << "\nProduction Bonus: "
          << mining.getProductionBonus()
          << endl;
-
-    cout << "Defense Bonus: "
-         << mining.getDefenseBonus()
-         << endl;
-
-    cout << "Attack Bonus: "
-         << mining.getAttackBonus()
-         << endl;
-
-    // Upgrade until max level
-
-    cout << "\nUpgrading to maximum level..." << endl;
-
-    while (mining.canUpgrade()) {
-
-        mining.upgrade();
-    }
-
-    mining.displayInfo();
-
-    cout << "\nCan Upgrade: ";
-
-    if (mining.canUpgrade()) {
-        cout << "Yes" << endl;
-    }
-    else {
-        cout << "No" << endl;
-    }
 
     return 0;
 }
