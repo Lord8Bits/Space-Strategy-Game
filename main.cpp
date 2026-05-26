0824776e3406b5f31950b847f636fc1f4eab4445
yassierr <najahnajah302@gmail.com>
2026-05-26 17:42:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,67 @@
-
 #include <iostream>
 #include "Resource.h"
 
 using namespace std;
 
-int main() {
-    // Create a Resource object
+int main()
+{
     Resource resources(100, 50, 25);
-    // Display initial valuess
-    cout << "INITIAL RESOURCES" << endl;
 
-    cout << "Gold : " << resources.getGold() << endl;
-    cout << "Silver : " << resources.getSilver() << endl;
-    cout << "Sodium : " << resources.getdiamond() << endl;
+    cout << "===== INITIAL RESOURCES =====" << endl;
 
-    // Add resources
+    cout << "Gold: "
+         << resources.getGold()
+         << endl;
+
+    cout << "Silver: "
+         << resources.getSilver()
+         << endl;
+
+    cout << "Diamond: "
+         << resources.getdiamond()
+         << endl;
+
+    // ADD RESOURCES
+
     resources.addGold(20);
     resources.addSilver(10);
     resources.adddiamond(5);
-    cout << endl;
-    
-    cout << "AFTER ADDING RESOURCES" << endl;
-    cout << "Gold : " << resources.getGold() << endl;
-    cout << "Silver : " << resources.getSilver() << endl;
-    cout << "Sodium : " << resources.getdiamond() << endl;
 
-    // Consume resources
+    cout << "\n===== AFTER ADDING =====" << endl;
+
+    cout << "Gold: "
+         << resources.getGold()
+         << endl;
+
+    cout << "Silver: "
+         << resources.getSilver()
+         << endl;
+
+    cout << "Diamond: "
+         << resources.getdiamond()
+         << endl;
+
+    // CONSUME RESOURCES
+
     resources.consumeGold(30);
     resources.consumeSilver(15);
     resources.consumediamond(10);
-    cout << endl;
-    
-    cout << "AFTER CONSUMING RESOURCES" << endl;
-    cout << "Gold: " << resources.getGold() << endl;
-    cout << "Silver: " << resources.getSilver() << endl;
-    cout << "Sodium: " << resources.getdiamond() << endl;
-    resources.update(); // And finally the update
+
+    cout << "\n===== AFTER CONSUMING =====" << endl;
+
+    cout << "Gold: "
+         << resources.getGold()
+         << endl;
+
+    cout << "Silver: "
+         << resources.getSilver()
+         << endl;
+
+    cout << "Diamond: "
+         << resources.getdiamond()
+         << endl;
+
+    resources.update();
+
     return 0;
 }
