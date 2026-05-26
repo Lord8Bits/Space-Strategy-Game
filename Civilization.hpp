7ad6fc2f6b7405695acf4c4812acbd8439e51b31
yassierr <najahnajah302@gmail.com>
2026-05-26 17:09:24 +0100

    Update Civilization.hpp

--- Civilization.hpp
+++ Civilization.hpp
@@ -3,7 +3,9 @@
 
 #include <string>
 #include <map>
-
+#include "../src/Utils/Enums.hpp"
+#include "../src/Utils/Constants.hpp"
+#include <unordered_map>
 #include "Resource.h"
 #include "Planet.h"
 #include "Updatable.hpp"
@@ -22,8 +24,26 @@ private:
     // value = Planet object
     map<string, Planet> planets;
     map<int, Technology> technologies;
-
+protected:
+    std::string _name;
+    CivilizationType type;
+    GameUI::Color color;
+    std::unordered_map<const Civilization*, Relation> _relations;
+    virtual Relation defaultRelationWith(const Civilization& other) const;
 public:
+    Civilization(CivilizationType Type) : type(Type), color(civToColor(Type)) {}
+    Civilization(CivilizationType Type);
+    virtual ~Civilization() = default;
+
+    void update() override;
+    void isAllyAttacked(const Civilization& ally, const Civilization* attacker);
+
+    GameUI::Color getColor() const { return color; }
+    
+    std::string getName() const { return _name; }
+    CivilizationType getCivType() const { return type; }
+    Relation getRelationWith(const Civilization& other) const;
+    void setRelationWith(const Civilization& other, Relation relation) { _relations[&other] = relation; }
 
     Civilization(string n, Resource r);
 
