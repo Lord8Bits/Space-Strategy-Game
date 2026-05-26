50df65a45bb9814398f4e0a7fb8e17a3a7a407f7
yassierr <najahnajah302@gmail.com>
2026-05-26 17:42:01 +0100

    Update Resource.cpp

--- Resource.cpp
+++ Resource.cpp
@@ -1,49 +1,76 @@
 #include "Resource.h"
-//Constructor
-Resource::Resource(int g, int s, int di) {
+
+// CONSTRUCTOR
+
+Resource::Resource(int g, int s, int di)
+{
     gold = g;
     silver = s;
     diamond = di;
 }
-//Getters
 
-int Resource::getGold() {
+// GETTERS
+
+int Resource::getGold()
+{
     return gold;
 }
-int Resource::getSilver() {
+
+int Resource::getSilver()
+{
     return silver;
 }
-int Resource::getdiamond() {
+
+int Resource::getdiamond()
+{
     return diamond;
 }
-//Add functions
 
-void Resource::addGold(int value) {
+// ADD FUNCTIONS
+
+void Resource::addGold(int value)
+{
     gold += value;
 }
-void Resource::addSilver(int value) {
+
+void Resource::addSilver(int value)
+{
     silver += value;
 }
-void Resource::adddiamond(int value) {
+
+void Resource::adddiamond(int value)
+{
     diamond += value;
 }
-// Consume functions
 
-void Resource::consumeGold(int value) {
-   if (gold >= value) {  // So that the value doesnt become negative
-    gold -= value;
-   }
+// CONSUME FUNCTIONS
+
+void Resource::consumeGold(int value)
+{
+    if (gold >= value)
+    {
+        gold -= value;
+    }
 }
-void Resource::consumeSilver(int value) {
-   if (silver >= value) {
-    silver -= value;
-   }  
+
+void Resource::consumeSilver(int value)
+{
+    if (silver >= value)
+    {
+        silver -= value;
+    }
 }
-void Resource::consumediamond(int value) {
-  if (diamond >= value) {
-    diamond -= value;
-  }
+
+void Resource::consumediamond(int value)
+{
+    if (diamond >= value)
+    {
+        diamond -= value;
+    }
 }
-// update function 
-void Resource::update() {
+
+// UPDATE
+
+void Resource::update()
+{
 }
