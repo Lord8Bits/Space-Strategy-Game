3b7fdf4af926516b3a7ee0479f60b5ba50ed41d1
yassierr <najahnajah302@gmail.com>
2026-05-26 17:41:29 +0100

    Update Resources.h

--- Resources.h
+++ Resources.h
@@ -1,47 +1,39 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
+
 #include "Updatable.hpp"
 
-// Resource class manages different types of resources (gold, silver, sodium)
+// Resource class manages different types of resources
 class Resource : public Updatable {
+
 private:
-    //ATTRIBUTES
-    int gold;    
-    int silver;  
-    int diamond;  
+
+    int gold;
+    int silver;
+    int diamond;
 
 public:
-    //CONSTRUCTOR
-    // Initializes the resource values
+
+    // CONSTRUCTOR
     Resource(int g, int s, int di);
 
-    //GETTERS 
-    int getGold();    
-    // returns the current amount of gold
-    int getSilver();  
-    // returns the current amount of silver
-    int getdiamond();  
-    // returns the current amount of sodium
+    // GETTERS
+    int getGold();
+    int getSilver();
+    int getdiamond();
 
-    //ADD FUNCTIONS
-    void addGold(int value);     
-    // adds "value" to gold
-    void addSilver(int value);   
-    // adds "value" to silver
-    void adddiamond(int value);   
-    // adds "value" to sodium
+    // ADD FUNCTIONS
+    void addGold(int value);
+    void addSilver(int value);
+    void adddiamond(int value);
 
-    //CONSUME FUNCTIONS 
-    void consumeGold(int value);    
-    //decreases the gold value
-    
+    // CONSUME FUNCTIONS
+    void consumeGold(int value);
+    void consumeSilver(int value);
+    void consumediamond(int value);
 
-    void consumeSilver(int value);  
-    // same logic as gold
-
-    void consumediamond(int value);  
-    // same logic as gold
-     void update() override;
+    // UPDATE
+    void update() override;
 };
 
 #endif
