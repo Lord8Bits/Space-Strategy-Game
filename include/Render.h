1cbd6b4012bdbb02561f51d94e69d99df4c63cf5
Lord8Bits <62944891+Lord8Bits@users.noreply.github.com>
2026-05-26 14:34:26 +0100

    Apply suggestion from @FengMin-000 : Cell Rendering
    Co-authored-by: FengMin-000 <PurSuerXx@proton.me>

--- include/Render.h
+++ include/Render.h
@@ -4,7 +4,10 @@
 #include "Entity.hpp"
 #include "Map.h"
 #include "ViewPort.h"
-
+struct Cell {
+    char symbol;
+    GameUI::Color color;
+};
 /// @brief Handles all rendering of the game world to the terminal
 ///
 /// Design decision: _viewport only stores one chunk at a time (CHUNK_SIZE cells)
